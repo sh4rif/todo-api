6f516ff66c2dcaf98a292f96089e5527b4507f0d
Muhammad Usman Sharif <sh4rif@hotmail.co.uk>
2018-08-13 10:14:59 +0100

    Added get todos routs

--- server/server.js
+++ server/server.js
@@ -24,6 +24,16 @@ app.post("/todos", (req, res) => {
     });
 });
 
+app.get("/todos", (req, res) => {
+  Todo.find()
+    .then(todos => {
+      res.send({ todos });
+    })
+    .catch(e => {
+      res.status(400).send(err);
+    });
+});
+
 app.listen(port, () => {
   console.log("Started on port ", port);
 });
