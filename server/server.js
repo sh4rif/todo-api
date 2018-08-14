4e0f273c9aec2a6d0080d029857a8282b5ed20b7
Muhammad Usman Sharif <sh4rif@hotmail.co.uk>
2018-08-14 04:07:14 +0100

    Added Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -61,10 +61,10 @@ app.delete("/todos/:id", (req, res) => {
   if (!ObjectID.isValid(id)) return res.status(404).send();
 
   Todo.findByIdAndRemove(id)
-    .then(doc => {
-      if (!doc) return res.status(404).send();
+    .then(todo => {
+      if (!todo) return res.status(404).send();
 
-      res.send(doc);
+      res.send({ todo });
     })
     .catch(err => {
       res.status(400).send();
