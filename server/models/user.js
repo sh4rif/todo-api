69553f88ff858439a6a1f7b6e1a152813842d3bb
Muhammad Usman Sharif <sh4rif@hotmail.co.uk>
2018-08-16 18:23:05 +0100

    Added DELETE /user/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,11 @@ UserSchema.methods.generateAuthToken = function() {
   return user.save().then(() => token);
 };
 
+UserSchema.methods.removeToken = function(token) {
+  var user = this;
+  return user.update({ $pull: { tokens: { token } } });
+};
+
 UserSchema.statics.findByToken = function(token) {
   var User = this;
   var decoded;
